5796b0aec82ab9054129d3460c5ead27fb03cde8
Panda64 <JaylenSchelb@yahoo.com>
2020-09-01 22:41:22 -0700

    Initial commit

---
+++ functions.py
@@ -0,0 +1,27 @@
+def area(width, height):
+    result = width * height
+    return result
+
+result = area(5, 6)
+print(result)
+
+result2 = area(3, 5)
+result3 = area(98, 32)
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+result4 = subtract(94, 4)
+result5 = subtract(30, 3)
+print(result4)
+print(result5)
+
+def add(num1, num2):
+    result = num1 + num2
+    return result
+
+result6 = add(3, 3)
+result7 = add(38, 5)
+print(result6)
+print(result7)
